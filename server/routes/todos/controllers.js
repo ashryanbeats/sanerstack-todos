291e2f6c60430f6c26da25a7c31a43b302fb07e3
Ash Ryan Arnwine <sayhi@ashryan.io>
2021-06-04 16:55:16 -0400

    Added express server.

---
+++ server/routes/todos/controllers.js
@@ -0,0 +1,56 @@
+const uuid = require("uuid");
+const asyncHandler = require("express-async-handler");
+const { getCollection } = require("../../database/astraClient");
+
+const getTodos = asyncHandler( async (req, res, next) => {
+    const todos = await getCollection();
+
+    try {
+        const astraRes = await todos.find({});
+        const formattedTodos = Object.keys(astraRes).map((item) => astraRes[item]);
+        res.send(formattedTodos);
+    } catch (error) {
+        next(error);
+    }
+});
+
+const createTodo = asyncHandler(async (req, res, next) => {
+    const todos = await getCollection();
+
+    try {
+        const astraRes = await todos.create(uuid.v1(), req.body);
+        res.send(astraRes);
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+const updateTodo = asyncHandler(async (req, res, next) => {
+    const todos = await getCollection();
+
+    try {
+        const astraRes = await todos.update(req.body.id, req.body);
+        res.send(astraRes);
+    } catch (error) {
+        next(error)
+    }
+});
+
+const deleteTodo = asyncHandler(async (req, res, next) => {
+    const todos = await getCollection();
+
+    try {
+        const astraRes = await todos.delete(req.body.id);
+        res.send(astraRes);
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = {
+  getTodos,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+};
