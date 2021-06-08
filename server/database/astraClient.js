d7d206f7db6a467ea72559ea3aa269a8e16b4a4e
Ash Ryan Arnwine <sayhi@ashryan.io>
2021-06-07 21:54:13 -0400

    Minor cleanup.

--- server/database/astraClient.js
+++ server/database/astraClient.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const { createClient } = require("@astrajs/collections");
 
 let astraClient = null;
@@ -9,15 +10,16 @@ const getAstraClient = async () => {
         astraDatabaseId: process.env.ASTRA_DB_ID,
         astraDatabaseRegion: process.env.ASTRA_DB_REGION,
         applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
-      },
-      30000
+      }
     );
   }
+
   return astraClient;
 };
 
 const getCollection = async () => {
   const documentClient = await getAstraClient();
+  
   return documentClient
     .namespace(process.env.ASTRA_DB_KEYSPACE)
     .collection("todos");
