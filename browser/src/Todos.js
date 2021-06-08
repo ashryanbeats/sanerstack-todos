377170374dabea5cb1d98328fb74f9023cd354ac
Ash Ryan Arnwine <sayhi@ashryan.io>
2021-06-07 22:29:01 -0400

    Mock create a new todo.

--- browser/src/Todos.js
+++ browser/src/Todos.js
@@ -11,13 +11,33 @@ function Todos() {
             const data = await res.json();
             setTodos(data);
         }
+
         fetchData();
     }, []);
+
+    function createTodo(e) {
+        e.preventDefault();
+
+        const raw = { text: `Made on ${Date.now()}`, completed: false };
+        const headers = {
+            'Content-Type': 'application/json'
+        };
+
+        async function postData() {
+            const options = { headers, method: "POST", body: JSON.stringify(raw) };
+            const res = await fetch("http://localhost:8000/api/v1/todos", options);
+            const data = await res.json();
+            setTodos([...todos, raw]);
+        }
+
+        postData();
+    }
 
 
     return (
         <div>
             Todos list
+            <button onClick={createTodo}>Create a todo</button>
             {todos.map((todo, i) => {
                 return <Todo key={i} todo={todo} />
             })}
